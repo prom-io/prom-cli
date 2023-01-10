a72bccc819309c9d067fe8345257045f9271d86f
Aleksei Dmitriev <alexey@ioncloud.ru>
2023-01-10 14:30:33 +0000

    feat: update trade marketplaces

--- src/config/marketplaces.ts
+++ src/config/marketplaces.ts
@@ -1,7 +1,7 @@
 import { ChainId } from "@/config/ChainId";
 
 export const marketplaces: Partial<Record<ChainId, string>> = {
-  [ChainId.Ethereum]: "0xaa3734c3ad2721bb54e404e454c1f8d4259ce239",
-  [ChainId.BSC]: "0x6Ed3580Fe5cD165F1D4694e3125e0cD7f084CA2B",
+  [ChainId.Ethereum]: "0x9bF0A8b1DB303c6A5333CFE07bDf58E4b9A8584a",
+  [ChainId.BSC]: "0xC1b3EdE61f1EAf379c315D72C8E94ef6c3b52981",
   [ChainId.Polygon]: "0x1ddFdcd35533dC639bA3387CddbB0Bc80738ffd2",
 };
