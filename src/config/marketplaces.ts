c3c36683e6f9e4a0357b277bf9e2f286916c93bb
georgyskr <gosha.skryuchenkov@gmail.com>
2023-02-08 15:59:10 +0200

    feat: update token configs

--- src/config/marketplaces.ts
+++ src/config/marketplaces.ts
@@ -4,5 +4,5 @@ export const marketplaces: Partial<Record<ChainId, string>> = {
   [ChainId.Ethereum]: "0x9bF0A8b1DB303c6A5333CFE07bDf58E4b9A8584a",
   [ChainId.BSC]: "0xC1b3EdE61f1EAf379c315D72C8E94ef6c3b52981",
   [ChainId.Polygon]: "0x1ddFdcd35533dC639bA3387CddbB0Bc80738ffd2",
-  [ChainId.Fantom]: "0x1ddFdcd35533dC639bA3387CddbB0Bc80738ffd2",
+  [ChainId.Fantom]: "0x6108692bE832D637854Ee3F735017d8B87B7Ed5e",
 };
